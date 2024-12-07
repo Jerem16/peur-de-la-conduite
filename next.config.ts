61dc923168ebe7fe852e0df6331810e2a3d4762c
Jerem16 <jeremy.lemaignent@gmail.com>
2024-12-07 13:57:42 +0100

    Base Header read

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    experimental: {
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Permissions-Policy",
+                        value: "geolocation=(), microphone=()",
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none';",
+                    },
+                ],
+            },
+        ];
     },
 };
 
