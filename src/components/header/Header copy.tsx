7fe56f72a25fd49488275aaf82f26b2005f1e52a
Jerem16 <jeremy.lemaignent@gmail.com>
2024-12-11 17:04:58 +0100

    feat: active-menu2

--- src/components/header/Header copy.tsx
+++ src/components/header/Header copy.tsx
@@ -5,58 +5,134 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Logo from "../svg_Icon/Logo";
 import Nav from "./NavLink";
-import { handleScrollClick, handleNavClick } from "../../utils/scrollUtils";
+import { handleScrollClick } from "../../utils/scrollUtils";
 import { useNavigation } from "../../utils/context/NavigationContext";
 
-// Définition des types pour les éléments de menu
-export interface SubItem {
+interface MenuItem {
+    id: string;
     title: string;
-    AnchorId: string;
-}
-
-export interface MenuItem {
-    title: string;
+    class: string;
     path: string;
-    subItems: SubItem[];
+    subItems: {
+        id: string;
+        title: string;
+        AnchorId: string;
+        class: string;
+    }[];
 }
 
 const Header: React.FC = () => {
     const pathname = usePathname();
     const { currentRoute, updateRoute } = useNavigation();
 
-    const handleNavigationClick = (path: string) => {
-        handleNavClick(path, currentRoute, updateRoute, handleScrollClick);
-    };
-
     const menuItems: MenuItem[] = [
         {
+            id: "menu-home",
             title: "Accueil",
+            class: "",
             path: "/",
             subItems: [
-                { title: "Slider", AnchorId: "#slider" },
-                { title: "À propos", AnchorId: "#about" },
-                { title: "Services", AnchorId: "#services" },
-                { title: "Contact", AnchorId: "#contact" },
+                {
+                    id: "menu-slider",
+                    title: "Slider",
+                    AnchorId: "#slider",
+                    class: "",
+                },
+                {
+                    id: "menu-about",
+                    title: "À propos",
+                    AnchorId: "#about",
+                    class: "",
+                },
+                {
+                    id: "menu-services",
+                    title: "Services",
+                    AnchorId: "#services",
+                    class: "",
+                },
+                {
+                    id: "menu-contact",
+                    title: "Contact",
+                    AnchorId: "#contact",
+                    class: "",
+                },
             ],
         },
         {
+            id: "menu-services",
             title: "Services",
+            class: "",
             path: "/page-services",
             subItems: [
-                { title: "Sans Permis", AnchorId: `#sans-permis` },
-                { title: "Avec Permis", AnchorId: "#avec-permis" },
+                {
+                    id: "menu-without-license",
+                    title: "Sans Permis",
+                    AnchorId: "#sans-permis",
+                    class: "",
+                },
+                {
+                    id: "menu-with-license",
+                    title: "Avec Permis",
+                    AnchorId: "#avec-permis",
+                    class: "",
+                },
             ],
         },
-        { title: "Blog", path: "/page-blog", subItems: [] },
-        { title: "Tarifs", path: "/page-tarifs", subItems: [] },
-        { title: "Contact", path: "/#contact", subItems: [] },
+        {
+            id: "menu-blog",
+            title: "Blog",
+            class: "",
+            path: "/page-blog",
+            subItems: [],
+        },
+        {
+            id: "menu-prices",
+            title: "Tarifs",
+            class: "",
+            path: "/page-tarifs",
+            subItems: [],
+        },
+        {
+            id: "menu-contact",
+            title: "Contact",
+            class: "",
+            path: "/#contact",
+            subItems: [],
+        },
     ];
+
+    const updateMenuClasses = (items: MenuItem[]): MenuItem[] => {
+        return items.map((item) => {
+            // Vérifiez si l'élément correspond à la route actuelle ou à une ancre sur la route actuelle
+            const isActive =
+                item.path === "/"
+                    ? currentRoute === "/" || currentRoute.startsWith("/#")
+                    : currentRoute.startsWith(item.path);
+
+            // Vérifiez si un sous-élément est actif
+            const activeSubItem = item.subItems.find((sub) =>
+                currentRoute.endsWith(sub.AnchorId)
+            );
+
+            return {
+                ...item,
+                class: isActive ? " active" : "",
+                subItems: item.subItems.map((sub) => ({
+                    ...sub,
+                    class: activeSubItem?.id === sub.id ? " active" : "",
+                })),
+            };
+        });
+    };
+
+    const updatedMenuItems = updateMenuClasses(menuItems);
 
     useEffect(() => {
         if (window.location.hash) {
             window.scrollTo({ top: 0 });
             handleScrollClick(window.location.hash.substring(1));
         }
+        console.log("Current Route:", currentRoute);
     }, [pathname]);
 
     return (
@@ -68,8 +144,8 @@ const Header: React.FC = () => {
                 <Logo />
             </Link>
             <Nav
-                menuItems={menuItems}
-                onNavigationClick={handleNavigationClick}
+                menuItems={updatedMenuItems}
+                onNavigationClick={(path: string) => updateRoute(path)}
             />
         </header>
     );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -103,20 +103,23 @@ const Header: React.FC = () => {
 
     const updateMenuClasses = (items: MenuItem[]): MenuItem[] => {
         return items.map((item) => {
+            // Vérifiez si l'élément correspond à la route actuelle ou à une ancre sur la route actuelle
             const isActive =
                 item.path === "/"
-                    ? currentRoute === item.path
+                    ? currentRoute === "/" || currentRoute.startsWith("/#")
                     : currentRoute.startsWith(item.path);
+
+            // Vérifiez si un sous-élément est actif
+            const activeSubItem = item.subItems.find((sub) =>
+                currentRoute.endsWith(sub.AnchorId)
+            );
 
             return {
                 ...item,
                 class: isActive ? " active" : "",
                 subItems: item.subItems.map((sub) => ({
                     ...sub,
-                    class:
-                        isActive && currentRoute.endsWith(sub.AnchorId)
-                            ? " active"
-                            : "",
+                    class: activeSubItem?.id === sub.id ? " active" : "",
                 })),
             };
         });
@@ -128,8 +131,8 @@ const Header: React.FC = () => {
         if (window.location.hash) {
             window.scrollTo({ top: 0 });
             handleScrollClick(window.location.hash.substring(1));
-            console.log("Current Route:", currentRoute);
         }
+        console.log("Current Route:", currentRoute);
     }, [pathname]);
 
     return (
