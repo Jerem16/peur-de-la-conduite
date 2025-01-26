1f39de5b9cbcea278204e86453f275b096efb759
Jerem16 <jeremy.lemaignent@gmail.com>
2025-01-26 02:18:09 +0100

    fix: (Search => Slider FTLoad)

--- app/search/SearchPageContent.tsx
+++ app/search/SearchPageContent.tsx
@@ -1,74 +1,54 @@
 "use client";
 
 import React, { useEffect, useState, useMemo } from "react";
-import { useRouter } from "next/navigation"; // Import du hook useRouter
+import { useRouter, useSearchParams } from "next/navigation";
 import { useSearch } from "../../src/utils/context/SearchContext";
 import searchQuery from "../../src/utils/searchMenu";
-import { useURLParams } from "../../src/utils/useURLParams";
 
 export default function SearchPageContent() {
     const router = useRouter();
     const { results, setResults, menuData, setQuery } = useSearch();
     const [validQuery, setValidQuery] = useState<string>("");
-    const { getParams } = useURLParams();
-    const badKeyWord = getParams("badKeyWord");
+    const searchParams = useSearchParams();
+    const badKeyWord = searchParams.get("badKeyWord");
+    const queryFromUrl = searchParams.get("query");
 
     useEffect(() => {
-        const queryFromUrl = getParams("query");
+        if (queryFromUrl && queryFromUrl !== validQuery) {
+            setValidQuery(queryFromUrl);
+            setQuery(queryFromUrl);
 
-        const newValidQuery = queryFromUrl || "";
-        if (newValidQuery !== validQuery) {
-            setValidQuery(newValidQuery);
-            setQuery(newValidQuery);
-        }
+            if (Array.isArray(menuData) && menuData.length > 0) {
+                const searchResults = searchQuery(menuData, queryFromUrl);
 
-        if (queryFromUrl && menuData && results.length === 0) {
-            const searchResults = searchQuery(menuData, queryFromUrl);
-
-            // Log des résultats de recherche
-            console.log("Search Results: ", searchResults);
-            console.log("Current Results: ", results);
-
-            if (JSON.stringify(searchResults) !== JSON.stringify(results)) {
                 setResults(searchResults);
             }
         }
-    }, [
-        getParams,
-        menuData,
-        validQuery,
-        badKeyWord,
-        setQuery,
-        setResults,
-        results,
-    ]);
+    }, [queryFromUrl, menuData, validQuery, setQuery, setResults]);
 
     const resultsCount = results.length;
     const validSearch = `${resultsCount} résultat${
         resultsCount > 1 ? "s" : ""
     } de recherche pour : "${validQuery}"`;
     const inValidSearch = `0 résultat pour "${badKeyWord}"`;
-
     const searchMessage = badKeyWord ? inValidSearch : validSearch;
 
-    const uniqueResults = useMemo(
-        () =>
-            results.filter(
-                (result, index, self) =>
-                    index ===
-                    self.findIndex(
-                        (r) =>
-                            r.path === result.path &&
-                            r.text.trim() === result.text.trim()
-                    )
-            ),
-        [results]
-    );
+    const uniqueResults = useMemo(() => {
+        return results.filter(
+            (result, index, self) =>
+                index ===
+                self.findIndex(
+                    (r) =>
+                        r.path === result.path &&
+                        r.text.trim() === result.text.trim()
+                )
+        );
+    }, [results]);
 
     return (
         <section className="section" id="s1">
             <div className="fixed-menu"></div>
-            <h2>{searchMessage}</h2>;
+            <h2>{searchMessage}</h2>
             <div className="s1">
                 {!badKeyWord ? (
                     uniqueResults.map((result) => (
@@ -76,7 +56,6 @@ export default function SearchPageContent() {
                             <button
                                 className="result-link"
                                 onClick={() => {
-                                    // Vérifier si result.go est défini
                                     if (result.go) {
                                         router.push(
                                             `${result.path}${result.go}`
@@ -91,9 +70,7 @@ export default function SearchPageContent() {
                         </div>
                     ))
                 ) : (
-                    <p>
-                        {badKeyWord ? `Aucun résultat à afficher.` : badKeyWord}
-                    </p>
+                    <p>Aucun résultat à afficher.</p>
                 )}
             </div>
         </section>
